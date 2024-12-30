45db14e01dd4f762d7cd05a832dc71121be311fb
aditya <aditya@bucle.dev>
2024-12-30 18:05:18 +0530

    basic folder setup

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ease_my_deal_assignment/presentation/main_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MainApp());
+}
---
+++ lib/presentation/main_app.dart
@@ -0,0 +1,21 @@
+import 'package:ease_my_deal_assignment/core/constants/strings.dart';
+import 'package:ease_my_deal_assignment/presentation/view/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget{
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:Strings.appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor:Colors.white,
+      ),
+      home:HomeScreen()
+    );
+      
+  }
+
+}

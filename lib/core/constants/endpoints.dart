47a765a7e1b26f4cf99700335010c145eaca8459
aditya <aditya@bucle.dev>
2024-12-30 18:37:33 +0530

    add: add endpoint and dio setup

---
+++ lib/core/constants/endpoints.dart
@@ -0,0 +1,7 @@
+class Endpoints {
+  Endpoints._();
+  static const String baseUrl="https://fakestoreapi.com";
+  static const int connectTimeout = 15000;
+    static const int receiveTimeout = 15000;
+
+}
---
+++ lib/core/dio_client/dio_client.dart
@@ -0,0 +1,149 @@
+
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:ease_my_deal_assignment/core/constants/endpoints.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioClient {
+  late final Dio _dio;
+
+  DioClient() {
+    _dio = Dio();
+    _dio
+      ..options.baseUrl = Endpoints.baseUrl
+      ..options.headers = {
+        HttpHeaders.contentTypeHeader: ContentType.json.mimeType,
+        HttpHeaders.authorizationHeader: '',
+      }
+      ..options.connectTimeout = const Duration(milliseconds: Endpoints.connectTimeout)
+      ..options.receiveTimeout = const Duration(milliseconds: Endpoints.receiveTimeout)
+      ..options.responseType = ResponseType.json
+      ..interceptors.add(
+        PrettyDioLogger(
+          compact: false,
+          logPrint: (object) => log(object.toString(), name: 'E-coomerce App '),
+        ),
+      );
+  }
+  /// * GET
+  Future<Response<dynamic>> get(
+    String url, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.get(
+        url,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+  /// * POST
+  Future<Response<dynamic>> post(
+    String uri, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.post(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+  /// * PUT
+  Future<Response<dynamic>> put(
+    String uri, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.put(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+  /// * PATCH
+  Future<Response<dynamic>> patch(
+    String uri, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.patch(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+  /// * DELETE
+  Future<dynamic> delete(
+    String uri, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.delete(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+}

30661dc0bfb8eb96fa0fa8338b24322d89d22190
aditya <aditya@bucle.dev>
2025-01-03 21:27:23 +0530

    mod: register app setting usecase

--- lib/domain/di/module/usecase_module.dart
+++ lib/domain/di/module/usecase_module.dart
@@ -1,5 +1,8 @@
 import 'package:ease_my_deal_assignment/di/service_locator.dart';
+import 'package:ease_my_deal_assignment/domain/repository/app_setting_repository.dart';
 import 'package:ease_my_deal_assignment/domain/repository/product_repository.dart';
+import 'package:ease_my_deal_assignment/domain/usecase/app_setting/get_app_setting_usecase.dart';
+import 'package:ease_my_deal_assignment/domain/usecase/app_setting/update_app_setting_usecase.dart';
 import 'package:ease_my_deal_assignment/domain/usecase/product/get_product_details_usecase.dart';
 import 'package:ease_my_deal_assignment/domain/usecase/product/get_product_usecase.dart';
 
@@ -13,6 +16,13 @@ mixin UseCaseModule {
 
     getIt.registerSingleton<GetProductDetailsUsecase>(
       GetProductDetailsUsecase(getIt<ProductRepository>()),
+    );
+
+    getIt.registerSingleton<GetAppSettingUsecase>(
+      GetAppSettingUsecase(getIt<AppSettingRepository>()),
+    );  
+    getIt.registerSingleton<UpdateAppSettingUsecase>(
+      UpdateAppSettingUsecase(getIt<AppSettingRepository>()),
     );  
     
   }
--- lib/domain/entity/app_setting.dart
+++ lib/domain/entity/app_setting.dart
@@ -15,19 +15,17 @@ class AppSetting {
     required this.padding,
   });
 
-  // Factory constructor to create an instance from JSON map
   factory AppSetting.fromJson(Map<String, dynamic> json) {
     return AppSetting(
       layout: Layout.values.firstWhere(
         (e) => e.name == json['layout'],
-        orElse: () => Layout.grid, // Default value if not found
+        orElse: () => Layout.grid, 
       ),
       theme: Theme.values.firstWhere((e)=> e.name== json['theme'], orElse: () => Theme.light,),
       padding: (json['padding'] as num).toDouble(),
     );
   }
 
-  // Method to convert an instance to a JSON map
   Map<String, dynamic> toMap() {
     return {
       'layout': layout.name,

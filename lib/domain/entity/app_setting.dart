0d375a5b44ccb821d3867d7b7fee2df471ecd4d0
aditya <aditya@bucle.dev>
2025-01-05 10:46:24 +0530

    mod: modification appSetting entity

--- lib/domain/entity/app_setting.dart
+++ lib/domain/entity/app_setting.dart
@@ -2,27 +2,39 @@ import 'dart:convert';
 
 // Enum for layout options
 enum Layout { single, grid }
-enum ThemeType {dark,light}
+enum ThemeType { dark, light }
 
 class AppSetting {
   final Layout layout;
   final ThemeType theme;
   final double padding;
+  final bool showSearchIcon;
+  final bool showWishListIcon;
+  final bool showCartIcon;
 
   AppSetting({
     required this.layout,
     required this.theme,
     required this.padding,
+    this.showSearchIcon = true,
+    this.showWishListIcon = true,
+    this.showCartIcon = true,
   });
 
   factory AppSetting.fromJson(Map<String, dynamic> json) {
     return AppSetting(
       layout: Layout.values.firstWhere(
         (e) => e.name == json['layout'],
-        orElse: () => Layout.grid, 
+        orElse: () => Layout.grid,
       ),
-      theme: ThemeType.values.firstWhere((e)=> e.name== json['theme'], orElse: () => ThemeType.light,),
-      padding: (json['padding'] as num).toDouble(),
+      theme: ThemeType.values.firstWhere(
+        (e) => e.name == json['theme'],
+        orElse: () => ThemeType.light,
+      ),
+      padding: (json['padding'] as num?)?.toDouble() ?? 8.0,
+      showSearchIcon: json['showSearchIcon'] ?? true,
+      showWishListIcon: json['showWishListIcon'] ?? true,
+      showCartIcon: json['showCartIcon'] ?? true,
     );
   }
 
@@ -31,24 +43,32 @@ class AppSetting {
       'layout': layout.name,
       'theme': theme.name,
       'padding': padding,
+      'showSearchIcon': showSearchIcon,
+      'showWishListIcon': showWishListIcon,
+      'showCartIcon': showCartIcon,
     };
   }
 
-  // Convert to JSON string
   String toJson() => json.encode(toMap());
 
-  // Factory constructor to create an instance from a JSON string
   factory AppSetting.fromJsonString(String source) =>
       AppSetting.fromJson(json.decode(source));
 
-
-    AppSetting copyWith({
+  AppSetting copyWith({
     Layout? layout,
     ThemeType? theme,
+    double? padding,
+    bool? showSearchIcon,
+    bool? showWishListIcon,
+    bool? showCartIcon,
   }) {
     return AppSetting(
       layout: layout ?? this.layout,
-      theme: theme ?? this.theme, padding: 8.0,
+      theme: theme ?? this.theme,
+      padding: padding ?? this.padding,
+      showSearchIcon: showSearchIcon ?? this.showSearchIcon,
+      showWishListIcon: showWishListIcon ?? this.showWishListIcon,
+      showCartIcon: showCartIcon ?? this.showCartIcon,
     );
-}
+  }
 }
